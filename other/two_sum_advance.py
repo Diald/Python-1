f098c1938ffdb67973c656fd9b39c317ad5831f0
Divya Gambhir <114723756+Diald@users.noreply.github.com>
2023-10-02 11:36:07 +0530

    Update two_sum_advance.py

--- other/two_sum_advance.py
+++ other/two_sum_advance.py
@@ -1,8 +1,12 @@
 '''<----------------------------------------2 POINTER SOLUTION --------------------------------------->
 
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+find two numbers such that they add up to a specific target number.
+Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+Return the indices of the two numbers, index1 and index2,
+added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. 
+You may not use the same element twice.
 Your solution must use only constant extra space.
 Example 1:
 
@@ -22,7 +26,7 @@ Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We ret
 
 
 #-------------------------CODE----------------# '''
-def two_sum(self, numbers: List[int], target: int) -> List[int]:
+def two_sum(self, numbers: list[int], target: int) -> list[int]:
     n = len(numbers)
     l = 0
     r = n-1
